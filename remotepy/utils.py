ba1580a47647fe63813e9dce763c3799269a6f48
Nick Sorros <nsorros@gmail.com>
2022-12-02 14:37:00 +0000

    Remove width calculation and rely on default auto

--- remotepy/utils.py
+++
@@ -1,9 +0,0 @@
-def get_column_widths(columns):
-    """Get the width of each column in a list of lists.
-    """
-    widths = []
-
-    for column in columns:
-        widths.append(max([len(str(i)) for i in column]))
-
-    return widths
