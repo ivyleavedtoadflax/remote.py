e2ac61021c2130c32cc348808ca890817d12eb02
Matthew Upson <matt@mantisnlp.com>
2023-07-15 13:40:35 +0200

    chg: Add new tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,7 @@
 import configparser
 
 import pytest
-from remotepy.cli import app
+from remotepy import config
 from typer.testing import CliRunner
 
 runner = CliRunner()
@@ -19,21 +19,28 @@ def test_config(tmpdir_factory):
     return config_path
 
 
-def test_add_default_instance_name(test_config):
-    result = runner.invoke(app, ["config", "add", "foobar", "-c", test_config])
-    assert result.exit_code == 0
-    assert "Default instance set to foobar" in result.stdout
+def test_create_config_dir_existing(mocker):
+    mocker.patch("os.path.exists", return_value=True)
+    mock_makedirs = mocker.patch("os.makedirs")
+    config.create_config_dir("dummy_path")
+    mock_makedirs.assert_not_called()
 
 
-def test_dont_update_default_instance_name(test_config):
-    result = runner.invoke(app, ["config", "add", "-c", test_config])
-    assert result.exit_code == 0
-    assert "No changes made" in result.stdout
+def test_create_config_dir_not_existing(mocker):
+    mocker.patch("os.path.exists", return_value=False)
+    mock_makedirs = mocker.patch("os.makedirs")
+    config.create_config_dir("dummy_path")
+    mock_makedirs.assert_called_once()
 
 
-def test_get_status_with_config(test_config):
-    result = runner.invoke(app, ["config", "show", "-c", test_config])
-    assert result.exit_code == 0
-    assert "DEFAULT" in result.stdout
-    assert "instance_name" in result.stdout
-    assert "test" in result.stdout
+def test_write_config(test_config, mocker):
+    mocker.patch("builtins.open", mocker.mock_open())
+    cfg = configparser.ConfigParser()
+    cfg["DEFAULT"]["instance_name"] = "test"
+    config.write_config(cfg, test_config)
+
+
+def test_add_no_instances(mocker):
+    mocker.patch("remotepy.config.get_instances", return_value=[])
+    result = runner.invoke(config.app, ["add"], input="1\n")
+    assert "Invalid number. No changes made" in result.stdout
