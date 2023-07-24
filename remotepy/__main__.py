d4ab35a5489347de80dc5b4cdd01f5a3505ddd90
Matthew Upson <matt@mantisnlp.com>
2023-07-24 18:15:16 +0200

    new: Add version command

--- remotepy/__main__.py
+++ remotepy/__main__.py
@@ -1,3 +1,7 @@
+import importlib.metadata
+
+import typer
+
 from remotepy.ami import app as ami_app
 from remotepy.config import app as config_app
 from remotepy.instance import app as app
@@ -6,6 +10,14 @@ from remotepy.volume import app as volume_app
 
 # This means that the instance app is the default app, so we don't need to run
 # remote instance, we can just run remote
+
+
+@app.command()
+def version():
+    """Show version."""
+    typer.echo(importlib.metadata.version("remotepy"))
+    typer.Exit(0)
+
 
 app.add_typer(ami_app, name="ami")
 app.add_typer(config_app, name="config")
