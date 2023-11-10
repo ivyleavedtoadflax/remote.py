90a9f07804f4501ec37c1bc9a8848d2f8d084493
Matthew Upson <matt@mantisnlp.com>
2023-11-10 18:29:05 +0100

    new: Add ecs module
    Adds commands:
    * list-clusters
    * list-services
    * scale

--- remotepy/__main__.py
+++ remotepy/__main__.py
@@ -7,6 +7,7 @@ from remotepy.config import app as config_app
 from remotepy.instance import app as app
 from remotepy.snapshot import app as snapshot_app
 from remotepy.volume import app as volume_app
+from remotepy.ecs import app as ecs_app
 
 # This means that the instance app is the default app, so we don't need to run
 # remote instance, we can just run remote
@@ -23,6 +24,7 @@ app.add_typer(ami_app, name="ami")
 app.add_typer(config_app, name="config")
 app.add_typer(snapshot_app, name="snapshot")
 app.add_typer(volume_app, name="volume")
+app.add_typer(ecs_app, name="ecs")
 
 if __name__ == "__main__":
     app()
